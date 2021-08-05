f8a6570a8fd8c68fe3d0f8a0b5494279441f14f9
SUDIPP22 <panja370@gmail.com>
2021-08-05 12:04:22 +0530

    Modified: Calc Of Equality Of Two Lines

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -4,16 +4,31 @@ public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome To Line Comparison Computation Program");
 
-        // constant
+
+        // coordinates of Line1
         int x1 = 10;
         int x2 = 20;
         int y1 = 30;
         int y2 = 40;
 
+        // coordinates of Line2
+        int a1 = 4;
+        int a2 = 6;
+        int b1 = 5;
+        int b2 = 7;
+
         // computation
 
-        double lengthOfLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        System.out.println("The Length is = " + lengthOfLine);
+        double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        double lengthOfLine2 = Math.sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
+
+        System.out.println("The Length Of Line1 is = " + lengthOfLine1);
+        System.out.println("The Length Of Line2 is = " + lengthOfLine2);
+
+        if (lengthOfLine1 == lengthOfLine2)
+            System.out.println("Two Lines are equal in length");
+        else
+            System.out.println("Lines are not equal in length");
 
     }
 }
