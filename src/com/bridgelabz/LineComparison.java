1674c93a01058dc11c357289e93d0bf8ef722c47
SUDIPP22 <panja370@gmail.com>
2021-08-05 11:45:09 +0530

    Added : Calc Of Length Of A Line

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -3,5 +3,17 @@ package com.bridgelabz;
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome To Line Comparison Computation Program");
+
+        // constant
+        int x1 = 10;
+        int x2 = 20;
+        int y1 = 30;
+        int y2 = 40;
+
+        // computation
+
+        double lengthOfLine = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        System.out.println("The Length is = " + lengthOfLine);
+
     }
 }
