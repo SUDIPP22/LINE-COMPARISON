930d765700ec1eaa8c7cf49e98f9ec0e2486162f
SUDIPP22 <panja370@gmail.com>
2021-08-05 17:08:11 +0530

    Refactored : Comparing Of Length Of Two Lines

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -19,16 +19,21 @@ public class LineComparison {
 
         // computation
 
-        double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-        double lengthOfLine2 = Math.sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
+        Double lengthOfLine1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+        Double lengthOfLine2 = Math.sqrt((a2 - a1) * (a2 - a1) + (b2 - b1) * (b2 - b1));
 
         System.out.println("The Length Of Line1 is = " + lengthOfLine1);
         System.out.println("The Length Of Line2 is = " + lengthOfLine2);
+        
+            // checking of comparison of two lines using compareTo method
 
-        if (lengthOfLine1 == lengthOfLine2)
-            System.out.println("Two Lines are equal in length");
+        if (lengthOfLine1.compareTo(lengthOfLine2) > 0)
+            System.out.println("Line1 is greater than Line2");
+        else if (lengthOfLine1.compareTo(lengthOfLine2) < 0)
+            System.out.println("Line1 is less than Line2");
         else
-            System.out.println("Lines are not equal in length");
+            System.out.println("Both lines are equal");
+
 
     }
 }
